13732fb3451a261b013c19d4758a982f1e1dfa5c
abhay-dharmale <abhaydharmale9@gmail.com>
2023-10-09 21:37:23 +0530

    some changes

--- script.js
+++ script.js
@@ -31,6 +31,28 @@ gsap.from('#page-1 nav', {
 })
 
 
+const tl1 = gsap.timeline({ repeat: -1 });
+tl1.to("#page-2 .main img", {
+    rotationY: "20", 
+    ease: "power1.inOut", 
+    duration: 1, 
+    repeat: -1, 
+    yoyo: true
+});
+
+gsap.from("#page-2 .main h3", {
+    x: -1400,
+    duration: 1,
+    stagger: 1,
+    scrollTrigger: {
+        trigger: "#page-2 .main h3",
+        scroller: "body",
+        start: "top 75%",
+        end: "top 55%",
+    }
+})
+
+
   gsap.from("#page-3 #info", {
     opacity: 0,
     scale: 0.6,
@@ -39,8 +61,30 @@ gsap.from('#page-1 nav', {
     scrollTrigger: { 
         trigger: "#page-3 #info", 
         scroller: "body",
-        start: "top 75%", 
-        end: "top 30%",
+        start: "top 82%", 
+        end: "top 35%",
         scrub: true,
     },
+  });
+
+  gsap.from("#page-4 .right .schedules", {
+    x: 750,
+    duration: 1,
+    delay: 1,
+    stagger: 0.5,
+    scrollTrigger: {
+        trigger: "#page-4 .right .schedules",
+        scroller: "body",
+        start: "top 80%",
+        end: "top 49%",
+        scrub: 2
+    }
+  })
+
+  gsap.to("#page-4 .left h1", {
+    y: 5, 
+    ease: "power3.in", 
+    duration: 1, 
+    repeat: -1, 
+    yoyo: true, 
   });
